09c6d03a0fb5b86ffb126d8d03a97cc42e87b259
Blizzeq <kubamax4@gmail.com>
2022-10-04 11:52:53 +0200

    Created Currencies object

---
+++ src/Components/Currencies.js
@@ -0,0 +1,815 @@
+const CurrenciesObject = {
+    AED: {
+        name: 'United Arab Emirates Dirham',
+        code: 'AED',
+        flag: 'fi fi-ae'
+    },
+    AFN: {
+        name: 'Afghan Afghani',
+        code: 'AFN',
+        flag: 'fi fi-af'
+    },
+    ALL: {
+        name: 'Albania',
+        code: 'ALL',
+        flag: 'fi fi-al'
+    },
+    AMD: {
+        name: 'Armenia',
+        code: 'AMD',
+        flag: 'fi fi-am'
+    },
+    ANG: {
+        name: 'Netherlands Antilles',
+        code: 'ANG',
+        flag: 'fi fi-aw'
+    },
+    AOA: {
+        name: 'Angola',
+        code: 'AOA',
+        flag: 'fi fi-ao'
+    },
+    ARS: {
+        name: 'Argentina',
+        code: 'ARS',
+        flag: 'fi fi-ar'
+    },
+    AUD: {
+        name: 'Australia',
+        code: 'AUD',
+        flag: 'fi fi-au'
+    },
+    AWG: {
+        name: 'Aruba',
+        code: 'AWG',
+        flag: 'fi fi-aw'
+    },
+    AZN: {
+        name: 'Azerbaijan',
+        code: 'AZN',
+        flag: 'fi fi-az'
+    },
+    BAM: {
+        name: 'Bosnia and Herzegovina',
+        code: 'BAM',
+        flag: 'fi fi-ba'
+    },
+    BBD: {
+        name: 'Barbados',
+        code: 'BBD',
+        flag: 'fi fi-bb'
+    },
+    BDT: {
+        name: 'Bangladesh',
+        code: 'BDT',
+        flag: 'fi fi-bd'
+    },
+    BGN: {
+        name: 'Bulgaria',
+        code: 'BGN',
+        flag: 'fi fi-bg'
+    },
+    BHD: {
+        name: 'Bahrain',
+        code: 'BHD',
+        flag: 'fi fi-bh'
+    },
+    BIF: {
+        name: 'Burundi',
+        code: 'BIF',
+        flag: 'fi fi-bi'
+    },
+    BMD: {
+        name: 'Bermuda',
+        code: 'BMD',
+        flag: 'fi fi-bm'
+    },
+    BND: {
+        name: 'Brunei Darussalam',
+        code: 'BND',
+        flag: 'fi fi-bn'
+    },
+    BOB: {
+        name: 'Bolivia',
+        code: 'BOB',
+        flag: 'fi fi-bo'
+    },
+    BRL: {
+        name: 'Brazil',
+        code: 'BRL',
+        flag: 'fi fi-br'
+    },
+    BSD: {
+        name: 'Bahamas',
+        code: 'BSD',
+        flag: 'fi fi-bs'
+    },
+    BTN: {
+        name: 'Bhutan',
+        code: 'BTN',
+        flag: 'fi fi-bt'
+    },
+    BWP: {
+        name: 'Botswana',
+        code: 'BWP',
+        flag: 'fi fi-bw'
+    },
+    BYN: {
+        name: 'Belarus',
+        code: 'BYN',
+        flag: 'fi fi-by'
+    },
+    BZD: {
+        name: 'Belize',
+        code: 'BZD',
+        flag: 'fi fi-bz'
+    },
+    CAD: {
+        name: 'Canada',
+        code: 'CAD',
+        flag: 'fi fi-ca'
+    },
+    CDF: {
+        name: 'Congo',
+        code: 'CDF',
+        flag: 'fi fi-cd'
+    },
+    CHF: {
+        name: 'Switzerland',
+        code: 'CHF',
+        flag: 'fi fi-ch'
+    },
+    CLP: {
+        name: 'Chilean Peso',
+        code: 'CLP',
+        flag: 'fi fi-cl'
+    },
+    CLF: {
+        name: 'Chilean Unidad de Fomento',
+        code: 'CLF',
+        flag: 'fi fi-cl'
+    },
+    CNY: {
+        name: 'China',
+        code: 'CNY',
+        flag: 'fi fi-cn'
+    },
+    COP: {
+        name: 'Colombia',
+        code: 'COP',
+        flag: 'fi fi-co'
+    },
+    CRC: {
+        name: 'Costa Rica',
+        code: 'CRC',
+        flag: 'fi fi-cr'
+    },
+    CUC: {
+        name: 'Cube Convertible Peso',
+        code: 'CUC',
+        flag: 'fi fi-cu'
+    },
+    CUP: {
+        name: 'Cuba',
+        code: 'CUP',
+        flag: 'fi fi-cu'
+    },
+    CVE: {
+        name: 'Cape Verde',
+        code: 'CVE',
+        flag: 'fi fi-cv'
+    },
+    CZK: {
+        name: 'Czech Republic',
+        code: 'CZK',
+        flag: 'fi fi-cz'
+    },
+    DJF: {
+        name: 'Djibouti',
+        code: 'DJF',
+        flag: 'fi fi-dj'
+    },
+    DKK: {
+        name: 'Denmark',
+        code: 'DKK',
+        flag: 'fi fi-dk'
+    },
+    DOP: {
+        name: 'Dominican Republic',
+        code: 'DOP',
+        flag: 'fi fi-do'
+    },
+    DZD: {
+        name: 'Algeria',
+        code: 'DZD',
+        flag: 'fi fi-dz'
+    },
+    EGP: {
+        name: 'Egypt',
+        code: 'EGP',
+        flag: 'fi fi-eg'
+    },
+    ERN: {
+        name: 'Eritrea',
+        code: 'ERN',
+        flag: 'fi fi-er'
+    },
+    ETB: {
+        name: 'Ethiopia',
+        code: 'ETB',
+        flag: 'fi fi-et'
+    },
+    EUR: {
+        name: 'Euro',
+        code: 'EUR',
+        flag: 'fi fi-eu'
+    },
+    FJD: {
+        name: 'Fiji',
+        code: 'FJD',
+        flag: 'fi fi-fj'
+    },
+    FKP: {
+        name: 'Falkland Islands',
+        code: 'FKP',
+        flag: 'fi fi-fk'
+    },
+    GBP: {
+        name: 'United Kingdom',
+        code: 'GBP',
+        flag: 'fi fi-gb'
+    },
+    GEL: {
+        name: 'Georgia',
+        code: 'GEL',
+        flag: 'fi fi-ge'
+    },
+    GHS: {
+        name: 'Ghana',
+        code: 'GHS',
+        flag: 'fi fi-gh'
+    },
+    GIP: {
+        name: 'Gibraltar',
+        code: 'GIP',
+        flag: 'fi fi-gi'
+    },
+    GMD: {
+        name: 'Gambia',
+        code: 'GMD',
+        flag: 'fi fi-gm'
+    },
+    GNF: {
+        name: 'Guinea',
+        code: 'GNF',
+        flag: 'fi fi-gn'
+    },
+    GTQ: {
+        name: 'Guatemala',
+        code: 'GTQ',
+        flag: 'fi fi-gt'
+    },
+    GYD: {
+        name: 'Guyana',
+        code: 'GYD',
+        flag: 'fi fi-gy'
+    },
+    HKD: {
+        name: 'Hong Kong',
+        code: 'HKD',
+        flag: 'fi fi-hk'
+    },
+    HNL: {
+        name: 'Honduras',
+        code: 'HNL',
+        flag: 'fi fi-hn'
+    },
+    HRK: {
+        name: 'Croatia',
+        code: 'HRK',
+        flag: 'fi fi-hr'
+
+    },
+    HTG: {
+        name: 'Haiti',
+        code: 'HTG',
+        flag: 'fi fi-ht'
+    },
+    HUF: {
+        name: 'Hungary',
+        code: 'HUF',
+        flag: 'fi fi-hu'
+    },
+    IDR: {
+        name: 'Indonesia',
+        code: 'IDR',
+        flag: 'fi fi-id'
+    },
+    ILS: {
+        name: 'Israel',
+        code: 'ILS',
+        flag: 'fi fi-il'
+    },
+    INR: {
+        name: 'India',
+        code: 'INR',
+        flag: 'fi fi-in'
+    },
+    IQD: {
+        name: 'Iraq',
+        code: 'IQD',
+        flag: 'fi fi-iq'
+    },
+    IRR: {
+        name: 'Iran',
+        code: 'IRR',
+        flag: 'fi fi-ir'
+    },
+    ISK: {
+        name: 'Iceland',
+        code: 'ISK',
+        flag: 'fi fi-is'
+    },
+    JMD: {
+        name: 'Jamaica',
+        code: 'JMD',
+        flag: 'fi fi-jm'
+    },
+    JOD: {
+        name: 'Jordan',
+        code: 'JOD',
+        flag: 'fi fi-jo'
+    },
+    JPY: {
+        name: 'Japan',
+        code: 'JPY',
+        flag: 'fi fi-jp'
+    },
+    KES: {
+        name: 'Kenya',
+        code: 'KES',
+        flag: 'fi fi-ke'
+    },
+    KGS: {
+        name: 'Kyrgyzstan',
+        code: 'KGS',
+        flag: 'fi fi-kg'
+    },
+    KHR: {
+        name: 'Cambodia',
+        code: 'KHR',
+        flag: 'fi fi-kh'
+    },
+    KMF: {
+        name: 'Comoros',
+        code: 'KMF',
+        flag: 'fi fi-km'
+    },
+    KPW: {
+        name: 'North Korea',
+        code: 'KPW',
+        flag: 'fi fi-kp'
+    },
+    KRW: {
+        name: 'South Korea',
+        code: 'KRW',
+        flag: 'fi fi-kr'
+    },
+    KWD: {
+        name: 'Kuwait',
+        code: 'KWD',
+        flag: 'fi fi-kw'
+    },
+    KYD: {
+        name: 'Cayman Islands',
+        code: 'KYD',
+        flag: 'fi fi-ky'
+    },
+    KZT: {
+        name: 'Kazakhstan',
+        code: 'KZT',
+        flag: 'fi fi-kz'
+    },
+    LAK: {
+        name: 'Laos',
+        code: 'LAK',
+        flag: 'fi fi-la'
+    },
+    LBP: {
+        name: 'Lebanon',
+        code: 'LBP',
+        flag: 'fi fi-lb'
+    },
+    LKR: {
+        name: 'Sri Lanka',
+        code: 'LKR',
+        flag: 'fi fi-lk'
+    },
+    LRD: {
+        name: 'Liberia',
+        code: 'LRD',
+        flag: 'fi fi-lr'
+    },
+    LSL: {
+        name: 'Lesotho',
+        code: 'LSL',
+        flag: 'fi fi-ls'
+    },
+    LYD: {
+        name: 'Libya',
+        code: 'LYD',
+        flag: 'fi fi-ly'
+    },
+    MAD: {
+        name: 'Morocco',
+        code: 'MAD',
+        flag: 'fi fi-ma'
+    },
+    MDL: {
+        name: 'Moldova',
+        code: 'MDL',
+        flag: 'fi fi-md'
+    },
+    MGA: {
+        name: 'Madagascar',
+        code: 'MGA',
+        flag: 'fi fi-mg'
+    },
+    MKD: {
+        name: 'Macedonia',
+        code: 'MKD',
+        flag: 'fi fi-mk'
+    },
+    MMK: {
+        name: 'Myanmar',
+        code: 'MMK',
+        flag: 'fi fi-mm'
+    },
+    MNT: {
+        name: 'Mongolia',
+        code: 'MNT',
+        flag: 'fi fi-mn'
+    },
+    MOP: {
+        name: 'Macau',
+        code: 'MOP',
+        flag: 'fi fi-mo'
+    },
+    MRU: {
+        name: 'Mauritania',
+        code: 'MRU',
+        flag: 'fi fi-mr'
+    },
+    MUR: {
+        name: 'Mauritius',
+        code: 'MUR',
+        flag: 'fi fi-mu'
+    },
+    MVR: {
+        name: 'Maldives',
+        code: 'MVR',
+        flag: 'fi fi-mv'
+    },
+    MWK: {
+        name: 'Malawi',
+        code: 'MWK',
+        flag: 'fi fi-mw'
+    },
+    MXN: {
+        name: 'Mexico',
+        code: 'MXN',
+        flag: 'fi fi-mx'
+    },
+    MYR: {
+        name: 'Malaysia',
+        code: 'MYR',
+        flag: 'fi fi-my'
+    },
+    MZN: {
+        name: 'Mozambique',
+        code: 'MZN',
+        flag: 'fi fi-mz'
+    },
+    NAD: {
+        name: 'Namibia',
+        code: 'NAD',
+        flag: 'fi fi-na'
+    },
+    NGN: {
+        name: 'Nigeria',
+        code: 'NGN',
+        flag: 'fi fi-ng'
+    },
+    NIO: {
+        name: 'Nicaragua',
+        code: 'NIO',
+        flag: 'fi fi-ni'
+    },
+    NOK: {
+        name: 'Norway',
+        code: 'NOK',
+        flag: 'fi fi-no'
+    },
+    NPR: {
+        name: 'Nepal',
+        code: 'NPR',
+        flag: 'fi fi-np'
+    },
+    NZD: {
+        name: 'New Zealand',
+        code: 'NZD',
+        flag: 'fi fi-nz'
+    },
+    OMR: {
+        name: 'Oman',
+        code: 'OMR',
+        flag: 'fi fi-om'
+    },
+    PAB: {
+        name: 'Panama',
+        code: 'PAB',
+        flag: 'fi fi-pa'
+    },
+    PEN: {
+        name: 'Peru',
+        code: 'PEN',
+        flag: 'fi fi-pe'
+    },
+    PGK: {
+        name: 'Papua New Guinea',
+        code: 'PGK',
+        flag: 'fi fi-pg'
+    },
+    PHP: {
+        name: 'Philippines',
+        code: 'PHP',
+        flag: 'fi fi-ph'
+    },
+    PKR: {
+        name: 'Pakistan',
+        code: 'PKR',
+        flag: 'fi fi-pk'
+    },
+    PLN: {
+        name: 'Poland',
+        code: 'PLN',
+        flag: 'fi fi-pl'
+    },
+    PYG: {
+        name: 'Paraguay',
+        code: 'PYG',
+        flag: 'fi fi-py'
+    },
+    QAR: {
+        name: 'Qatar',
+        code: 'QAR',
+        flag: 'fi fi-qa'
+    },
+    RON: {
+        name: 'Romania',
+        code: 'RON',
+        flag: 'fi fi-ro'
+    },
+    RSD: {
+        name: 'Serbia',
+        code: 'RSD',
+        flag: 'fi fi-rs'
+    },
+    RUB: {
+        name: 'Russia',
+        code: 'RUB',
+        flag: 'fi fi-ru'
+    },
+    RWF: {
+        name: 'Rwanda',
+        code: 'RWF',
+        flag: 'fi fi-rw'
+    },
+    SAR: {
+        name: 'Saudi Arabia',
+        code: 'SAR',
+        flag: 'fi fi-sa'
+    },
+    SBD: {
+        name: 'Solomon Islands',
+        code: 'SBD',
+        flag: 'fi fi-sb'
+    },
+    SCR: {
+        name: 'Seychelles',
+        code: 'SCR',
+        flag: 'fi fi-sc'
+    },
+    SDG: {
+        name: 'Sudan',
+        code: 'SDG',
+        flag: 'fi fi-sd'
+    },
+    SEK: {
+        name: 'Sweden',
+        code: 'SEK',
+        flag: 'fi fi-se'
+    },
+    SGD: {
+        name: 'Singapore',
+        code: 'SGD',
+        flag: 'fi fi-sg'
+    },
+    SHP: {
+        name: 'Saint Helena',
+        code: 'SHP',
+        flag: 'fi fi-sh'
+    },
+    SLL: {
+        name: 'Sierra Leone',
+        code: 'SLL',
+        flag: 'fi fi-sl'
+    },
+    SOS: {
+        name: 'Somalia',
+        code: 'SOS',
+        flag: 'fi fi-so'
+    },
+    SRD: {
+        name: 'Suriname',
+        code: 'SRD',
+        flag: 'fi fi-sr'
+    },
+    STN: {
+        name: 'Sao Tome and Principe',
+        code: 'STN',
+        flag: 'fi fi-st'
+    },
+    SVC: {
+        name: 'El Salvador',
+        code: 'SVC',
+        flag: 'fi fi-sv'
+    },
+    SYP: {
+        name: 'Syria',
+        code: 'SYP',
+        flag: 'fi fi-sy'
+    },
+    SZL: {
+        name: 'Swaziland',
+        code: 'SZL',
+        flag: 'fi fi-sz'
+    },
+    THB: {
+        name: 'Thailand',
+        code: 'THB',
+        flag: 'fi fi-th'
+    },
+    TJS: {
+        name: 'Tajikistan',
+        code: 'TJS',
+        flag: 'fi fi-tj'
+    },
+    TMT: {
+        name: 'Turkmenistan',
+        code: 'TMT',
+        flag: 'fi fi-tm'
+    },
+    TND: {
+        name: 'Tunisia',
+        code: 'TND',
+        flag: 'fi fi-tn'
+    },
+    TOP: {
+        name: 'Tonga',
+        code: 'TOP',
+        flag: 'fi fi-to'
+    },
+    TRY: {
+        name: 'Turkey',
+        code: 'TRY',
+        flag: 'fi fi-tr'
+    },
+    TTD: {
+        name: 'Trinidad and Tobago',
+        code: 'TTD',
+        flag: 'fi fi-tt'
+    },
+    TWD: {
+        name: 'Taiwan',
+        code: 'TWD',
+        flag: 'fi fi-tw'
+    },
+    TZS: {
+        name: 'Tanzania',
+        code: 'TZS',
+        flag: 'fi fi-tz'
+    },
+    UAH: {
+        name: 'Ukraine',
+        code: 'UAH',
+        flag: 'fi fi-ua'
+    },
+    UGX: {
+        name: 'Uganda',
+        code: 'UGX',
+        flag: 'fi fi-ug'
+    },
+    USD: {
+        name: 'United States',
+        code: 'USD',
+        flag: 'fi fi-us'
+    },
+    UYU: {
+        name: 'Uruguay',
+        code: 'UYU',
+        flag: 'fi fi-uy'
+    },
+    UZS: {
+        name: 'Uzbekistan',
+        code: 'UZS',
+        flag: 'fi fi-uz'
+    },
+    VES: {
+        name: 'Venezuela',
+        code: 'VES',
+        flag: 'fi fi-ve'
+    },
+    VND: {
+        name: 'Vietnam',
+        code: 'VND',
+        flag: 'fi fi-vn'
+    },
+    VUV: {
+        name: 'Vanuatu',
+        code: 'VUV',
+        flag: 'fi fi-vu'
+    },
+    WST: {
+        name: 'Samoa',
+        code: 'WST',
+        flag: 'fi fi-ws'
+    },
+    XAF: {
+        name: 'Cameroon',
+        code: 'XAF',
+        flag: 'fi fi-cm'
+    },
+    XAG: {
+        name: 'Silver',
+        code: 'XAG',
+        flag: 'fi fi-xag'
+    },
+    XAU: {
+        name: 'Gold',
+        code: 'XAU',
+        flag: 'fi fi-xau'
+    },
+    XCD: {
+        name: 'East Caribbean',
+        code: 'XCD',
+        flag: 'fi fi-xcd'
+    },
+    XDR: {
+        name: 'Special Drawing Rights',
+        code: 'XDR',
+        flag: 'fi fi-xdr'
+    },
+    XOF: {
+        name: 'CFA Franc BCEAO',
+        code: 'XOF',
+        flag: 'fi fi-xof'
+    },
+    XPD: {
+        name: 'Palladium',
+        code: 'XPD',
+        flag: 'fi fi-xpd'
+    },
+    XPF: {
+        name: 'CFP Franc',
+        code: 'XPF',
+        flag: 'fi fi-xpf'
+    },
+    XPT: {
+        name: 'Platinum',
+        code: 'XPT',
+        flag: 'fi fi-xpt'
+    },
+    YER: {
+        name: 'Yemen',
+        code: 'YER',
+        flag: 'fi fi-ye'
+    },
+    ZAR: {
+        name: 'South Africa',
+        code: 'ZAR',
+        flag: 'fi fi-za'
+    },
+    ZMW: {
+        name: 'Zambia',
+        code: 'ZMW',
+        flag: 'fi fi-zm'
+    },
+    ZWL: {
+        name: 'Zimbabwe',
+        code: 'ZWL',
+        flag: 'fi fi-zw'
+    }
+}
+
+export default CurrenciesObject;
